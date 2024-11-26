cd71d1e596937f87512c1ecf7a8fd54cef12c51e
filipbaranski <f.g.baranski@gmail.com>
2024-11-26 03:50:57 +0100

    Fix: Last attempt at CORS today

--- middleware.js
+++ middleware.js
@@ -2,7 +2,27 @@ const Users = require("./models/Users");
 // const jwt = require('jsonwebtoken');
 const jose = require("jose");
 
-module.exports = () => {
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+  );
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({
+      body: "OK",
+    });
+  }
   return async (req, res, next) => {
     const token = req.headers["bearer"];
     const id = req.headers["id"];

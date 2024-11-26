dffb591c0980db3931c0f3912843094b1002a309
filipbaranski <f.g.baranski@gmail.com>
2024-11-26 03:08:47 +0100

    Fix: Fixed README files

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 1. git add .
 2. git commit -m ""
 3. git push
-4. Test
--- api/index.js
+++ api/index.js
@@ -4,14 +4,7 @@ const app = express();
 const routes = require("../routes");
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 

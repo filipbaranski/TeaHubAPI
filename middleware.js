da7b9ce3fabfd50df6cdd35700ae38ed10856cfa
filipbaranski <f.g.baranski@gmail.com>
2024-11-26 03:58:19 +0100

    Fix: Revert to safe build

--- middleware.js
+++ middleware.js
@@ -4,26 +4,6 @@ const jose = require("jose");
 
 module.exports = () => {
   return async (req, res, next) => {
-    //set header first to allow request or origin domain (value can be different)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, OPTIONS, DELETE"
-    );
-
-    //---- other code
-
-    //Preflight CORS handler
-    // if (req.method === "OPTIONS") {
-    return res.status(200).json({
-      body: "OK",
-    });
-    // }
     const token = req.headers["bearer"];
     const id = req.headers["id"];
     const user = await Users.findOne({ _id: id });
@@ -45,7 +25,7 @@ module.exports = () => {
         token,
         new TextEncoder().encode(process.env.JWT_SECRET)
       );
-      // console.log(payload);
+      console.log(payload);
       // jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
       if (!payload) {
         return res.status(401).send({ error: "Invalid token" });

2ee6a5a242d2c41b7d31361027aca66f817c273c
filipbaranski <f.g.baranski@gmail.com>
2024-11-26 02:31:36 +0100

    Feature: Added further cors enablers

--- api/index.js
+++ api/index.js
@@ -4,7 +4,14 @@ const app = express();
 const routes = require("../routes");
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 app.use(routes);
 

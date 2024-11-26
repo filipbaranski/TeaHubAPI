d5122702470b05bc7810a56ec8aecf424dd49fa2
filipbaranski <f.g.baranski@gmail.com>
2024-11-26 01:36:50 +0100

    Fix: Changed Atlas connection string

--- routes.js
+++ routes.js
@@ -8,11 +8,13 @@ const userController = require('./controllers/user');
 const datesController = require('./controllers/dates');
 const calendarController = require('./controllers/calendar');
 
-mongoose.connect(process.env.DB_CONNECTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-});
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_CONNECTION_URI);
+}
 
 router.post('/signin', userController.signIn);
 router.get('/signout/:id', userController.signOut);
